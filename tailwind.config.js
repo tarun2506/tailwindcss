5e9867d4f6b0fc47d26a91cdf1348ed8637f99ff
Tarun Sharma <tarunsharma8920@gmail.com>
2021-09-22 20:43:04 +0530

    overviewed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5928E5",
+        secondary: {
+          100: "#8357ff",
+          200: "#3e02e8",
+        },
+      },
+      fontFamily: {
+        body: ["Poppins"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
